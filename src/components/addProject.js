d9c6e7942dad03ae7b641ef96f6f6b46fb5605f0
shreyasdoda <shreyurd@gmail.com>
2020-07-07 02:38:13 +0530

    ->Changed favicon

--- src/components/addProject.js
+++ src/components/addProject.js
@@ -1,16 +1,18 @@
 import {Input, Button, Checkbox, Dropdown, Header, Confirm} from 'semantic-ui-react';
 import CKEditor from '@ckeditor/ckeditor5-react';
-// import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import InlineEditor from '@ckeditor/ckeditor5-build-inline';
 import React, { Component } from 'react';
 import axios from 'axios';
 import '../styles/form.css';
 import MyUploadAdapter from "../uploadAdapter";
+import "cropperjs/dist/cropper.min.css";
+import Cropper from "cropperjs";
 
 class AddProject extends Component {
 
     constructor(props) {
         super(props);
+        this.original_image_reference = React.createRef();
         this.state={
             userList: [],
             editor_images: [],
@@ -22,27 +24,13 @@ class AddProject extends Component {
             project_slug: "",
             slug_available:"",
             slug_valid: true,
+            cropped_image_url: "https://react.semantic-ui.com/images/avatar/large/matthew.png",
+            original_image_url:"",
             submit_loading: false,
             confirm_open: false,
             editorID: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
         }
     }
-
-    // getCookie(cname) {
-    //   var name = cname + "=";
-    //   var decodedCookie = decodeURIComponent(document.cookie);
-    //   var ca = decodedCookie.split(';');
-    //   for(var i = 0; i <ca.length; i++) {
-    //     var c = ca[i];
-    //     while (c.charAt(0) == ' ') {
-    //       c = c.substring(1);
-    //     }
-    //     if (c.indexOf(name) == 0) {
-    //       return c.substring(name.length, c.length);
-    //     }
-    //   }
-    //   return "";
-    // }
 
     componentDidMount() {
 
@@ -67,6 +55,17 @@ class AddProject extends Component {
                 })
             }
         );
+
+        // const cropper = new Cropper(this.original_image_reference.current,{
+        //     zoomable: false,
+        //     scalable: false,
+        //     aspectRatio: 1,
+        //     crop: () => {
+        //         alert("wor")
+        //         const canvas = cropper.getCroppedCanvas();
+        //         this.setState({cropped_image_url: canvas.toDataURL("image/png")});
+        //     }
+        // });
 
     }
 
@@ -117,16 +116,41 @@ class AddProject extends Component {
     uploadImage = (e) => {
         let image = e.target.files[0];
 
-        let extension = image.name.split('.').pop();
+        if(image){
+            this.setState({
+                project_image: image,
+            });
+            // // alert("running")
+            // let container = document.getElementById("image-preview");
+            // container.style.display = "flex";
+            //
+            // let extension = image.name.split('.').pop();
+            // if(!(extension === "png" || extension === "jpg" || extension === "jpeg")){
+            //     alert("Image is not mandatory, but if uploaded, must be of file type .jpeg, .png, or .jpg .");
+            //     return;
+            // }
+            //
+            // const reader = new FileReader();
+            // reader.addEventListener("load", () => {
+            //     console.log("in listener")
+            //     console.log(reader.result)
+            //     let res = reader.result;
+            //     this.setState({
+            //         project_image: image,
+            //         original_image_url: res,
+            //     })
+            // });
+            //
+            // reader.readAsDataURL(image);
 
-        if(!(extension === "png" || extension === "jpg" || extension === "jpeg")){
-            alert("Image is not mandatory, but if uploaded, must be of file type .jpeg, .png, or .jpg .");
-            return;
+        } else{
+            this.setState({
+                project_image: null,
+                original_image_url: "",
+            });
+            let container = document.getElementById("image-preview");
+            container.style.display = null;
         }
-
-        this.setState({
-            project_image: image,
-        });
     }
 
     submitForm() {
@@ -291,7 +315,11 @@ class AddProject extends Component {
 
 
                 <Header style={{marginTop:"25px"}} as={'h3'}>Upload Image:</Header>
-                <input style={{marginTop:"0px"}} type="file" id="image-upload" onChange={this.uploadImage}/>
+                <input style={{marginTop:"0px"}} type="file" accept="image/*" id="image-upload" onChange={this.uploadImage}/>
+                {/*<div className="image-preview" id="image-preview">*/}
+                {/*            <div><img ref={this.original_image_reference} alt="Uploaded Image" src={this.state.original_image_url} id={"image-preview__original-image"} className="image-preview__original-image"/></div>*/}
+                {/*            <div><img alt="Cropped Image" src={this.state.cropped_image_url} className="image-preview__cropped-image"/></div>*/}
+                {/*</div>*/}
 
                 <Checkbox id="deployed-or-not"
                           label='Project has already been deployed'
