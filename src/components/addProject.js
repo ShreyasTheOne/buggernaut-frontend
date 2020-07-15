e1a32d5977283be059cfd12cf7d7db38e7e701e9
shreyasdoda <shreyurd@gmail.com>
2020-07-16 02:49:03 +0530

    An attempt at cropping images

--- src/components/addProject.js
+++ src/components/addProject.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {Editor} from "@tinymce/tinymce-react";
 import axios from 'axios';
 import '../styles/form.css';
-import "cropperjs/dist/cropper.min.css";
+
 import {
     urlApiDeleteRemainingImages, urlApiImages,
     urlApiProjects,
@@ -13,12 +13,24 @@ import {
     urlAppDashboardDeployedProjects
 } from "../urls";
 
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+import {
+    base64StringToFile, downloadBase64File,
+    extractImageFileExtensionFromBase64,
+    image64toCanvasRef
+} from "../imageCropperHelperFunctions";
+const imageMaxSize = 20000000 // bytes
+const acceptedFileTypes = 'image/png, image/jpg, image/jpeg';
+const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item) => {return item.trim()});
+
 class AddProject extends Component {
 
     constructor(props) {
         super(props);
-        this.original_image_reference = React.createRef();
-        this.state={
+        this.fileInputRef = React.createRef()
+        this.imagePreviewCanvasRef = React.createRef()
+        this.state = {
             userList: [],
             editor_images: [],
             project_image: null,
@@ -27,10 +39,11 @@ class AddProject extends Component {
             project_deployed: false,
             project_members: [],
             project_slug: "",
-            slug_available:"",
+            slug_available: "",
             slug_valid: true,
-            cropped_image_url: "https://react.semantic-ui.com/images/avatar/large/matthew.png",
-            original_image_url:"",
+            imgSrc: null,
+            imgSrcExt: null,
+            crop: {aspect: 3/2 , unit: "px",},
             submit_loading: false,
             confirm_open: false,
             editorID: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
@@ -47,7 +60,7 @@ class AddProject extends Component {
             (response) => {
                 let arr = [];
                 const ul = response.data;
-                for(let user in ul){
+                for (let user in ul) {
                     let dict = {};
                     dict["key"] = user;
                     dict["value"] = ul[user]["pk"];
@@ -60,18 +73,6 @@ class AddProject extends Component {
                 })
             }
         );
-
-        // const cropper = new Cropper(this.original_image_reference.current,{
-        //     zoomable: false,
-        //     scalable: false,
-        //     aspectRatio: 1,
-        //     crop: () => {
-        //         alert("wor")
-        //         const canvas = cropper.getCroppedCanvas();
-        //         this.setState({cropped_image_url: canvas.toDataURL("image/png")});
-        //     }
-        // });
-
     }
 
     onProjectNameChange() {
@@ -79,27 +80,27 @@ class AddProject extends Component {
         let slug = value.replace(/\s+/g, '-').toLowerCase();
         let validator = /^[a-z0-9]+(?:-[a-z0-9]+)*$/;
 
-        if(!validator.test(slug)){
+        if (!validator.test(slug)) {
             this.setState({
                 slug_valid: false, project_name: value, project_slug: slug, slug_available: false
             });
             return;
-        }else{
+        } else {
             this.setState({
                 slug_valid: true,
             });
         }
 
-        if(!(value === "") ){
+        if (!(value === "")) {
             let url = urlApiProjectVerifySlug(slug);
             axios({
                 method: 'get',
-                url:url,
+                url: url,
                 withCredentials: true,
             }).then(
                 (response) => {
                     let status = response.data["status"];
-                    if(status === "Available"){
+                    if (status === "Available") {
                         this.setState({
                             project_name: value, project_slug: slug, slug_available: true
                         });
@@ -118,43 +119,96 @@ class AddProject extends Component {
 
     }
 
-    uploadImage = (e) => {
-        let image = e.target.files[0];
+    getCroppedImage(){
+        const imgSrc = this.state.imgSrc
+        if (imgSrc) {
+            const canvasRef = this.imagePreviewCanvasRef.current
 
-        if(image){
-            this.setState({
-                project_image: image,
-            });
-            // // alert("running")
-            // let container = document.getElementById("image-preview");
-            // container.style.display = "flex";
-            //
-            // let extension = image.name.split('.').pop();
-            // if(!(extension === "png" || extension === "jpg" || extension === "jpeg")){
-            //     alert("Image is not mandatory, but if uploaded, must be of file type .jpeg, .png, or .jpg .");
-            //     return;
-            // }
-            //
-            // const reader = new FileReader();
-            // reader.addEventListener("load", () => {
-            //     console.log("in listener")
-            //     console.log(reader.result)
-            //     let res = reader.result;
-            //     this.setState({
-            //         project_image: image,
-            //         original_image_url: res,
-            //     })
-            // });
-            //
-            // reader.readAsDataURL(image);
+            const {imgSrcExt} =  this.state
+            const imageData64 = canvasRef.toDataURL('image/' + imgSrcExt)
 
-        } else{
-            this.setState({
-                project_image: null,
-                original_image_url: "",
-            });
-            let container = document.getElementById("image-preview");
-            container.style.display = null;
+            const myFilename = this.state.project_name + "." + imgSrcExt;
+            // file to be uploaded
+            const myNewCroppedFile = base64StringToFile(imageData64, myFilename)
+            console.log("myNewCroppedFile")
+            console.log(myNewCroppedFile)
+            // return file
+            return myNewCroppedFile;
+        }
+    }
+
+    verifyFile = (files) => {
+        if (files && files.length > 0) {
+            const currentFile = files[0]
+            const currentFileType = currentFile.type
+            const currentFileSize = currentFile.size
+            if (currentFileSize > imageMaxSize) {
+                alert("This file is not allowed. " + currentFileSize + " bytes is too large")
+                return false
+            }
+            if (!acceptedFileTypesArray.includes(currentFileType)) {
+                alert("This file is not allowed. Only images are allowed.")
+                return false
+            }
+            return true
+        }
+    }
+
+    handleImageLoad = (image) => {
+        console.log(image)
+    }
+
+    handleOnCrop = (crop) => {
+        this.setState({crop: crop});
+    }
+
+    handleOnCropComplete = (crop, pixelCrop) => {
+        console.log(crop, pixelCrop);
+        const canvas = this.imagePreviewCanvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const imgSrc = this.state.imgSrc;
+        canvas.width = crop.width;
+        canvas.height = crop.height;
+        // const scaleY = image.naturalHeight / image.height;
+        const image = new Image()
+        image.src = imgSrc
+        image.onload = function () {
+            ctx.drawImage(
+                image,
+                crop.x,
+                crop.y,
+                crop.width,
+                crop.height,
+                0,
+                0,
+                crop.width,
+                crop.height,
+            )
+        }
+    }
+
+    handleUploadImage = (e) => {
+        let files = e.target.files;
+        if (files && files.length > 0) {
+            const isVerified = this.verifyFile(files)
+            if (isVerified) {
+                // imageBase64Data
+                const currentFile = files[0]
+                const myFileItemReader = new FileReader()
+                myFileItemReader.addEventListener("load", () => {
+                    // console.log("myFileItemReader.result")
+                    // console.log(myFileItemReader.result)
+                    const myResult = myFileItemReader.result
+
+                    this.setState({
+                        imgSrc: myResult,
+                        imgSrcExt: extractImageFileExtensionFromBase64(myResult)
+                    })
+                    // alert(this.state.imgSrc)
+                    // alert(this.state.imgSrcExt)
+                }, false)
+                myFileItemReader.readAsDataURL(currentFile)
+            }
         }
     }
 
@@ -192,12 +246,19 @@ class AddProject extends Component {
             return;
         }
 
-        if(this.state.project_image === null){
+        // let projectImage = this.getCroppedImage();
+        let projectImage;
+        // console.log("this.state.imgSrc")
+        // console.log(this.state.imgSrc)
+        if(this.state.imgSrc === null){
             let del = window.confirm("You have not uploaded an image, and a default image will be set." +
                 "\nAre you ready to continue?");
             if(!del){return;}
+        } else {
+            projectImage = this.getCroppedImage();
         }
-        let projectImage = this.state.project_image;
+
+        // return;
 
         let formData = new FormData();
         if(projectImage !== null) formData.append('image', projectImage);
@@ -372,12 +433,28 @@ class AddProject extends Component {
                           }}/>
 
 
-                <Header style={{marginTop:"25px"}} as={'h3'}>Upload Image:</Header>
-                <input style={{marginTop:"0px"}} type="file" accept="image/*" id="image-upload" onChange={this.uploadImage}/>
-                {/*<div className="image-preview" id="image-preview">*/}
-                {/*            <div><img ref={this.original_image_reference} alt="Uploaded Image" src={this.state.original_image_url} id={"image-preview__original-image"} className="image-preview__original-image"/></div>*/}
-                {/*            <div><img alt="Cropped Image" src={this.state.cropped_image_url} className="image-preview__cropped-image"/></div>*/}
-                {/*</div>*/}
+                <Header style={{marginTop:"25px"}} as={'h3'}>Upload Display Image:</Header>
+                <input
+                    style={{marginTop:"0px"}}
+                    type="file"
+                    multiple={false}
+                    accept="image/png, image/jpg, image/jpeg"
+                    id="image-upload"
+                    onChange={this.handleUploadImage}/>
+
+                {this.state.imgSrc !== null &&
+                    <div className="image-crop-div">
+                        <ReactCrop
+                            id="original-image"
+                            src={this.state.imgSrc}
+                            crop={this.state.crop}
+                            onChange={this.handleOnCrop}
+                            onImageLoaded={this.handleImageLoad}
+                            onComplete={this.handleOnCropComplete}
+                            />
+                        <canvas id="preview-cropped-image" ref={this.imagePreviewCanvasRef}/>
+                    </div>
+                }
 
                 <Checkbox id="deployed-or-not"
                           label='Project has already been deployed'
@@ -393,6 +470,7 @@ class AddProject extends Component {
                        loading={this.state.submit_loading}
                        secondary
                        onClick={() => {this.setState({confirm_open: true})}}>
+                       {/*onClick={() => {this.getCroppedImage()}}>*/}
                        Submit
                    </Button>
                 </div>
